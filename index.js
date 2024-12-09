0735f989287b396ddd46481021fbb60b529e67d1
talant <talant.04asanov@gmail.com>
2024-12-09 11:08:57 +0600

    fix: client urls

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
     credentials: true,
-    origin: process.env.CLIENT_URL
+    origin: [process.env.CLIENT_URL, process.env.VERCEL_CLIENT_URL, process.env.CLIENT_BUILD_URL]
 }))
 app.use('/api', router)
 app.use(errorMiddleware)
