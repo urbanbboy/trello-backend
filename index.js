6eecfdd6a8f7901276a909a69b688736cdff4ff6
talant <talant.04asanov@gmail.com>
2024-12-18 12:10:03 +0600

    fix url

--- index.js
+++ index.js
@@ -15,8 +15,16 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
     credentials: true,
-    origin: [process.env.CLIENT_URL, process.env.VERCEL_CLIENT_URL, process.env.CLIENT_BUILD_URL]
+    // origin: [process.env.CLIENT_URL, process.env.VERCEL_CLIENT_URL, process.env.CLIENT_BUILD_URL]
 }))
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Origin', process.env.VERCEL_CLIENT_URL);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
 
 app.use('/api', router)
 app.use(errorMiddleware)
